0c77fd99134d291a32074e1863e395b0701098e5
Student <student@jcysewski.local>
2016-04-05 11:01:34 -0500

    Initial Commit

---
+++ Lolla LineupTests/Lolla_LineupTests.swift
@@ -0,0 +1,36 @@
+//
+//  Lolla_LineupTests.swift
+//  Lolla LineupTests
+//
+//  Created by jcysewski on 4/5/16.
+//  Copyright © 2016 jcysewski. All rights reserved.
+//
+
+import XCTest
+@testable import Lolla_Lineup
+
+class Lolla_LineupTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
